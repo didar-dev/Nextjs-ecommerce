5b53f23f35f1db05a2c464ddb2688dad485cacf3
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-10 01:27:17 +0300

    login and register

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,94 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../../utils/mongoConnect";
+import User from "../../../models/User";
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+type Data = {
+  error: string;
+  success: string;
+  token: string;
+  user: any;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({
+      error: "Please add all the fields",
+      success: "",
+      token: "",
+      user: undefined,
+    });
+  }
+  let existingUser: any;
+  connectDB()
+    .then(async () => {
+      try {
+        existingUser = await User.findOne({ email: email });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          error: "Something went wrong",
+          success: "",
+          token: "",
+          user: undefined,
+        });
+      }
+      if (!existingUser) {
+        res.status(422).json({
+          error: "User does not exist",
+          success: "",
+          token: "",
+          user: undefined,
+        });
+      }
+      let isValidPassword = false;
+      try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({
+          error: "Something went wrong",
+          success: "",
+          token: "",
+          user: undefined,
+        });
+      }
+      if (!isValidPassword) {
+        res.status(422).json({
+          error: "Invalid credentials",
+          success: "",
+          token: "",
+          user: undefined,
+        });
+      }
+      const token = jwt.sign(
+        {
+          id: existingUser._id,
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+      const Profile = {
+        id: existingUser._id,
+        name: existingUser.name,
+        email: existingUser.email,
+      };
+      res.status(200).json({
+        success: "User logged in",
+        token: token,
+        error: "",
+        user: Profile,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,46 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import connectDB from '../../../utils/mongoConnect'
-import User from '../../../models/User'
-type Data = {
-  name: string
-  error: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../../utils/mongoConnect";
+import User from "../../../models/User";
+const bcrypt = require("bcryptjs");
 
-export default function handler(
+type Data = {
+  name: string;
+  error: string;
+};
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const { email, password, name } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({
+      error: "Please add all the fields",
+      name: "Error",
+    });
+  }
 
-    const { email, password , name } = req.body
-    if (!email || !password || !name) {
-      return res.status(422).json({
-          error: 'Please add all the fields',
-          name: 'Error'
-      })
-    }
-   connectDB().then(() => {
-    const CreatedUser = new User({
-        email,
-        password,
-        name
-    })
-    CreatedUser.save().then((user: any) => {
-        res.status(200).json({
-            name: 'User created',
-            error: ''
+  /// hash password
+  const hashedPassword = await bcrypt.hash(password, 12);
+  connectDB()
+    .then(() => {
+      const CreatedUser = new User({
+        email: email,
+        password: hashedPassword,
+        name: name,
+      });
+      CreatedUser.save()
+        .then((user: any) => {
+          res.status(200).json({
+            name: "User created",
+            error: "",
+          });
         })
-        }).catch((err: any) => {
-            console.log(err)
-            res.status(500).json({
-                name: 'Error creating user',
-                error: ''
-            })
-        }
-    )
- 
-  }).catch((err) => {
-    console.log(err)
-  })
-
+        .catch((err: any) => {
+          console.log(err);
+          res.status(500).json({
+            name: "Error creating user",
+            error: "",
+          });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
