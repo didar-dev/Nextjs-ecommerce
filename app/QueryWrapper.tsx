06b0ff9cf567c4507da968f43c7f39c379d57292
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-16 23:49:08 +0300

    update

--- app/QueryWrapper.tsx
+++ app/QueryWrapper.tsx
@@ -1,15 +1,11 @@
 "use client";
-
 import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "react-hot-toast";
-
 const queryClient = new QueryClient();
-
 interface Props {
   children?: ReactNode;
 }
-
 const QueryWrapper = ({ children }: Props) => (
   <QueryClientProvider client={queryClient}>
     <Toaster
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../prisma/client";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "example@didar.dev",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (user && user.password === credentials.password) {
+          return user;
+        }
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
