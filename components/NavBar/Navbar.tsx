23d5e940553b84cf45b8995d2958cf82ed0e0e35
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-02-28 21:04:49 +0300

    navbar

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -8,99 +8,10 @@ export default function Example() {
   const { User } = useGlobalContext();
 
   return (
-    <div>
-      <nav className="w-full bg-gray-800 shadow">
-        <div className="justify-between px-2 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-          <div>
-            <div className="flex items-center justify-between py-2  md:block">
-              <Link href="/">
-                <h2 className="text-2xl text-white font-bold">Shoppy</h2>
-              </Link>
-              <div className="md:hidden">
-                <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? "block" : "hidden"
-              }`}
-            >
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-white">
-                  <Link href="/">
-                    <p className="text-white">Home</p>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/blogs">
-                    <p className="text-white">Blogs</p>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/about">
-                    <p className="text-white">About</p>
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/contact">
-                    <p className="text-white">Contact</p>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            {User ? (
-              <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                <Link href="/profile">
-                  <h1 className=" text-white font-bold">{User?.Name}</h1>
-                </Link>
-              </div>
-            ) : (
-              <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                <Link href="/Login">
-                  <h1 className=" text-white font-bold">Login</h1>
-                </Link>
-              </div>
-            )}
-          </div>
-        </div>
-      </nav>
+    <div className="bg-gray-900 h-12 w-full md:w-3/4  grid grid-cols-3 mx-auto">
+      <div className="flex  justify-between items-center h-full"></div>
+      <div className="flex  justify-between items-center h-full"></div>
+      <div className="flex  justify-between items-center h-full"></div>
     </div>
   );
 }
